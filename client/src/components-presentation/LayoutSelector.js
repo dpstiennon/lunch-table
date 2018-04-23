5f20af50bb63dd169ff3b6684897c96653fba195
Stiennon, David <David.Stiennon@ampf.com>
2018-04-23 08:32:58 -0500

    more layout component

--- client/src/components-presentation/LayoutSelector.js
+++ client/src/components-presentation/LayoutSelector.js
@@ -5,13 +5,21 @@ const LayoutSelector = ({teacherName, layouts}) => {
     alert(id)
   }
 
-  return <div className="layouts">
-    <h1>{teacherName}</h1>
-    {layouts.map((layout) =>
-    <div className="layout" onClick={() => navigateAway(layout.id)}>
-      <h3>{layout.name}</h3>
-    </div>)}
+  const newLayoutModal = () => {
+    alert('plus')
+  }
 
+  return <div className="layouts">
+    <div className="teacherHeader">
+      <h1>{teacherName}</h1>
+    </div>
+    <div className="layouts">
+      {layouts.map((layout) =>
+        <div className="layout" onClick={() => navigateAway(layout.id)}>
+          <h3>{layout.name}</h3>
+        </div>)}
+      <div className="layout" onClick={newLayoutModal}></div>
+    </div>
   </div>
 
 }
