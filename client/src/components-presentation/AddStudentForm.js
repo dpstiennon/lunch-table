3b415dd2864ca2d76a9a9d48362ebb60a52ee1d9
Stiennon, David <David.Stiennon@ampf.com>
2018-09-04 13:31:36 -0500

    Peanut radio

--- client/src/components-presentation/AddStudentForm.js
+++ client/src/components-presentation/AddStudentForm.js
@@ -6,49 +6,66 @@ import RadioGroup from '@material-ui/core/RadioGroup'
 import Radio from '@material-ui/core/Radio'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
 
-const AddStudentForm = (props) => {
-  const { createStudent } = props
-  return <div className="row">
-    <form onSubmit={createStudent}>
-      <div className="form-group">
-        <label htmlFor="firstName">First Name</label>
-        <input className="form-control form-control-sm" type="text" name="firstName" placeholder="Last Name"/>
-      </div>
-      <div className="form-group">
-        <label htmlFor="lastName">Last Name</label>
-        <input className="form-control form-control-sm" type="text" name="firstName" placeholder="First Name"/>
-      </div>
-      <div className="form-group">
-        <label htmlFor="Birth date">Birth Date</label>
-        <TextField
-          id="date"
-          name="Birth date"
-          className="form-control"
-          type="date"
-          defaultValue="2017-05-24"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="boyGirl">Boy</label>
-        <Switch name="boyGirl"
-        color="secondary"/>
-        <label htmlFor="boyGirl">Girl</label>
-      </div>
-      <div className="form-group">
-        <RadioGroup>
-          <FormControlLabel value="peanut" control={<Radio />} label="Peanut" />
-          <FormControlLabel value="noPeanutOk" control={<Radio />} label="No Peanut - okay" />
-          <FormControlLabel value="noPeanut" control={<Radio />} label="No Peanut" />
-        </RadioGroup>
-      </div>
-      <Button variant="contained" color="primary">
-        Create
-      </Button>
-    </form>
-  </div>
+class AddStudentForm extends Component {
+  constructor () {
+    super()
+    this.state = {
+      peanut: ''
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  setPeanut (event) {
+    this.setState({peanut: event.target.value})
+  }
+
+  render () {
+    const {createStudent} = this.props
+    return <div className="">
+      <form onSubmit={createStudent}>
+        <div className="form-group">
+          <label htmlFor="firstName">First Name</label>
+          <input className="form-control form-control-sm" type="text" name="firstName" placeholder="Last Name"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="lastName">Last Name</label>
+          <input className="form-control form-control-sm" type="text" name="firstName" placeholder="First Name"/>
+        </div>
+        <div className="form-group">
+          <label htmlFor="Birth date">Birth Date</label>
+          <TextField
+            id="date"
+            name="Birth date"
+            className="form-control"
+            type="date"
+            defaultValue="2017-05-24"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="boyGirl">Boy</label>
+          <Switch name="boyGirl"
+                  color="secondary"/>
+          <label htmlFor="boyGirl">Girl</label>
+        </div>
+        <div className="form-group">
+          <RadioGroup onChange={(e) => {this.setPeanut(e)}} value={this.state.peanut}>
+            <FormControlLabel value="peanut" control={<Radio/>} label="Peanut"/>
+            <FormControlLabel value="noPeanutOk" control={<Radio/>} label="No Peanut - okay"/>
+            <FormControlLabel value="noPeanut" control={<Radio/>} label="No Peanut"/>
+          </RadioGroup>
+        </div>
+        <Button variant="contained" color="primary">
+          Create
+        </Button>
+      </form>
+    </div>
+  }
 }
 
 export default AddStudentForm
