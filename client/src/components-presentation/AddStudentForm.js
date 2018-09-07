e22243155653ff5724d2670d14c8a3dde03a2581
Stiennon, David <David.Stiennon@ampf.com>
2018-09-06 20:54:36 -0500

    More refining of Add Student form

--- client/src/components-presentation/AddStudentForm.js
+++ client/src/components-presentation/AddStudentForm.js
@@ -1,29 +1,34 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Button from '@material-ui/core/Button'
 import TextField from '@material-ui/core/TextField'
 import Switch from '@material-ui/core/Switch'
 import RadioGroup from '@material-ui/core/RadioGroup'
 import Radio from '@material-ui/core/Radio'
 import FormControlLabel from '@material-ui/core/FormControlLabel'
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles'
 
 const styles = (theme) => {
   return {
-  newStudentForm: {
-    display: 'flex',
-    justifyContent: 'space-around'
-  },
-  formControl: {
-    flex: '1 1 400px',
-    padding: '20px'
-  },
-  blueSwitch: {
-    backgroundColor: theme.palette.primary.main
-  },
-  pinkSwitch: {
-    backgroundColor: theme.palette.secondary.main
+    newStudentForm: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap'
+    },
+    formControl: {
+      flex: '1 1 160px',
+      padding: 20,
+      marginTop: 16,
+      borderWidth: 2,
+      borderColor: 'black',
+      borderRadius: 5
+    },
+    boySwitch: {
+      backgroundColor: theme.palette.primary.main
+    },
+    girlSwitch: {
+      backgroundColor: theme.palette.secondary.main
+    }
   }
-}
 }
 
 class AddStudentForm extends Component {
@@ -32,18 +37,23 @@ class AddStudentForm extends Component {
     this.state = {
       peanut: '',
       birthDate: '',
-      firstName: ''
+      firstName: '',
+      boyGirl: 0
     }
   }
 
   updateState = (key) => {
-    return (e) => this.setState({ [key]: e.target.value })
+    return (e) => {
+      this.setState({[key]: e.target.value})
+    }
   }
+
+  updateSwitch = e => this.setState({boyGirl: e.target.checked})
 
   render () {
     const {createStudent, classes} = this.props
-    const {birthDate, peanut, firstName, lastName} = this.state
-    return <form onSubmit={createStudent} >
+    const {birthDate, peanut, firstName, lastName, boyGirl} = this.state
+    return <form onSubmit={createStudent}>
       <div className={classes.newStudentForm}>
         <div className={classes.formControl}>
           <TextField
@@ -53,7 +63,7 @@ class AddStudentForm extends Component {
             margin="normal"
           />
         </div>
-        <div className={classes.formControl} >
+        <div className={classes.formControl}>
           <TextField
             onChange={this.updateState('lastName')}
             value={lastName}
@@ -61,7 +71,21 @@ class AddStudentForm extends Component {
             margin="normal"
           />
         </div>
-        <div className={classes.formControl} >
+
+        <div className={classes.formControl}>
+          <label htmlFor="boyGirl">Boy</label>
+          <Switch name="boyGirl"
+                  classes={{
+                    icon: classes.boySwitch,
+                    bar: boyGirl ? classes.girlSwitch : classes.boySwitch,
+                    iconChecked: classes.girlSwitch
+                  }}
+                  checked={boyGirl}
+                  onChange={this.updateSwitch}
+                  color="secondary"/>
+          <label htmlFor="boyGirl">Girl</label>
+        </div>
+        <div className={classes.formControl}>
           <TextField
             onChange={this.updateState('birthDate')}
             value={birthDate}
@@ -72,18 +96,7 @@ class AddStudentForm extends Component {
             }}
           />
         </div>
-        <div className={classes.formControl} >
-          <label htmlFor="boyGirl">Boy</label>
-          <Switch name="boyGirl"
-                  classes={{
-                    icon: classes.blueSwitch,
-                    bar: classes.blueSwitch,
-                    iconChecked: classes.pinkSwitch
-                  }}
-                  color="secondary"/>
-          <label htmlFor="boyGirl">Girl</label>
-        </div>
-        <div className={classes.formControl} >
+        <div className={classes.formControl}>
           <RadioGroup onChange={this.updateState('peanut')} value={peanut}>
             <FormControlLabel value="peanut" control={<Radio/>} label="Peanut"/>
             <FormControlLabel value="noPeanutOk" control={<Radio/>} label="No Peanut - okay"/>
@@ -91,8 +104,8 @@ class AddStudentForm extends Component {
           </RadioGroup>
         </div>
       </div>
-      <Button variant="contained" color="primary">
-        Create
+      <Button variant="contained" color="primary" type="submit">
+          Create
       </Button>
     </form>
   }
