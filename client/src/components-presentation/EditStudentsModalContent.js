9158d70122b037afcb4bb9b6e6f86da96d715656
Stiennon, David <David.Stiennon@ampf.com>
2018-10-28 16:20:46 -0500

    Filling out edit student modal

--- client/src/components-presentation/EditStudentsModalContent.js
+++ client/src/components-presentation/EditStudentsModalContent.js
@@ -3,6 +3,8 @@ import { PeanutIcon } from './PeanutIcon'
 import Button from '@material-ui/core/Button/Button'
 import { tableStyles } from '../jss/formStyles'
 import withStyles from '@material-ui/core/es/styles/withStyles'
+import Checkbox from '@material-ui/core/Checkbox/Checkbox'
+import SortFilter from './SortFilter'
 
 class EditStudentsModalContent extends Component {
   fullName(student) {
@@ -13,14 +15,24 @@ class EditStudentsModalContent extends Component {
     const {students, layoutStudentIds, classes} = this.props
     return (
       <div>
+        <div className={classes.studentRow}>
+          <span className={classes.studentData}><SortFilter/></span>
+          <span className={classes.studentData}><SortFilter/></span>
+          <span className={classes.studentData}><SortFilter/></span>
+          <span className={classes.studentData}><SortFilter/></span>
+        </div>
         {students.map(student => (
           <div key={student.id} className={classes.studentRow}>
+            <span className={classes.studentData}><Checkbox color="primary"/></span>
             <span className={classes.studentData}>{student.fullName()}</span>
             <span className={classes.studentData}>{student.sex()}</span>
             <span className={classes.studentData}>{student.currentGrade()}</span>
-            <PeanutIcon peanut={student.peanut}/>
+            <span className={classes.studentData}><PeanutIcon peanut={student.peanut}/></span>
           </div>
         ))}
+        <div className={ classes.studentRow }>
+          <Button variant="contained" color="primary">Add</Button>
+        </div>
       </div>
     )
   }
---
+++ client/src/components-presentation/SortFilter.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SortFilter () {
+  return (
+    <div>
+      <input type="text" />
+      <Arrow/>
+    </div>
+  )
+}
+
+function Arrow () {
+  return (
+    <span>Down</span>
+  )
+}
+
+export default SortFilter
